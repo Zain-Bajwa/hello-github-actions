48ab777065d530184f457babc77403fc5be0e674
Zain-Bajwa <i191288@nu.edu.pk>
2022-07-28 15:11:57 +0500

    8

--- src/test_first.py
+++ src/test_first.py
@@ -4,15 +4,24 @@
 
 
 def test_function():
+    """test_function
+    """
+
     assert 2 + 4 == 6
 
 
 def test_function1():
+    """test_function1
+    """
+
     string = "Ali"
     assert string == "Ali"
 
 
 def test_function2():
+    """test_function2
+    """
+
     string = "Ali"
     print(string)
     assert string == "Ali"
