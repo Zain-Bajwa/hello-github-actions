fc88c1514158e2a4e32dfa2bdd4a3b399c0d2ac7
Zain-Bajwa <i191288@nu.edu.pk>
2022-07-28 15:06:54 +0500

    sixth commit

---
+++ src/test_first.py
@@ -0,0 +1,17 @@
+"""Testing File
+
+"""
+
+def test_function():
+    assert 2 + 4 == 6
+
+
+def test_function1():
+    string = "Ali"
+    assert string == "Ali"
+
+
+def test_function2():
+    string = "Ali"
+    print(string)
+    assert string == "Ali"
